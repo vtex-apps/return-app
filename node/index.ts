872e1b1e1f31ab92f279b6cf577e1cd4f9fa193d
Cristian Longo <cristian.longo@customsoft.ro>
2020-12-11 18:07:22 +0200

    Settings Admin

---
+++ node/index.ts
@@ -0,0 +1,55 @@
+import {ClientsConfig, Service, ServiceContext, method, LRUCache, RecorderState} from '@vtex/api'
+
+import { Clients } from './clients'
+import { returnAppGetSchemas } from './middlewares/returnAppGetSchemas'
+import { generateReturnsSchema } from './middlewares/generateReturnsSchema'
+import { receiveDocuments } from './middlewares/receiveDocuments'
+import { receiveCategories } from './middlewares/receiveCategories'
+import { saveMasterdataDocuments } from './middlewares/saveMasterdataDocuments'
+import { updateMasterdataDocuments } from './middlewares/updateMasterdataDocuments'
+
+const TIMEOUT_MS = 5000
+const memoryCache = new LRUCache<string, any>({ max: 5000 })
+
+const clients: ClientsConfig<Clients> = {
+  implementation: Clients,
+  options: {
+    default: {
+      retries: 2,
+      timeout: TIMEOUT_MS,
+    },
+    status: {
+      memoryCache
+    }
+  },
+}
+
+declare global {
+  type Context = ServiceContext<Clients, State>
+
+  interface State extends RecorderState {}
+}
+
+export default new Service({
+  clients,
+  routes: {
+    getSchemas: method({
+      GET: returnAppGetSchemas
+    }),
+    generateSchema: method({
+      PUT: generateReturnsSchema
+    }),
+    getDocuments: method({
+      GET: receiveDocuments
+    }),
+    getCategories: method({
+      GET: receiveCategories
+    }),
+    saveDocuments: method({
+      POST: saveMasterdataDocuments
+    }),
+    updateDocuments: method({
+      PUT: updateMasterdataDocuments
+    })
+  },
+})
--- react/store/MyReturnsPage.tsx
+++ react/store/MyReturnsPage.tsx
@@ -1,39 +1,40 @@
 import React from "react";
 import { ContentWrapper } from "vtex.my-account-commons";
+import { EmptyState } from "vtex.styleguide";
+import { FormattedMessage } from "react-intl";
+
 import { PageProps } from "../typings/utils";
-import { EmptyState } from 'vtex.styleguide'
-import { FormattedMessage } from 'react-intl'
-import extraProps from '../extra/extraProps'
+import extraProps from "../extra/extraProps";
 
 const MyReturnsPage = (props: PageProps) => {
-    const { returnsList, headerConfig } = props
+  const { returnsList, headerConfig } = props;
 
   return (
     <ContentWrapper {...headerConfig}>
-        {() => {
-            let jsx
+      {() => {
+        let jsx;
 
-            // eslint-disable-next-line prettier/prettier
+        // eslint-disable-next-line prettier/prettier
             if (returnsList?.length) {
-                jsx = returnsList.map((returnId: string) => (
-                    // eslint-disable-next-line no-console
-                    console.log(returnId)
-                ))
-            } else {
-                jsx = [
-                    <EmptyState key="empty-state" title="">
-                        <p>
-                            <FormattedMessage id="store/my-returns.no_returns" />
-                        </p>
-                    </EmptyState>,
-                ]
-            }
-            return (
-                <div className="flex-ns flex-wrap-ns items-start-ns relative tl">
-                    {jsx}
-                </div>
-            )
-        }}
+          jsx = returnsList.map((returnId: string) =>
+            // eslint-disable-next-line no-console
+            console.log(returnId)
+          );
+        } else {
+          jsx = [
+            <EmptyState key="empty-state" title="">
+              <p>
+                <FormattedMessage id="store/my-returns.no_returns" />
+              </p>
+            </EmptyState>
+          ];
+        }
+        return (
+          <div className="flex-ns flex-wrap-ns items-start-ns relative tl">
+            {jsx}
+          </div>
+        );
+      }}
     </ContentWrapper>
   );
 };
