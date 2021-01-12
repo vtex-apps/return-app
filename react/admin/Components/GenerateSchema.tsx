e92f4414d91d50156984e407adb4586ee211967a
Dan Nedelcu <dan.nedelcu@customsoft.ro>
2021-01-12 15:52:26 +0200

    ref #52625

--- react/admin/Components/GenerateSchema.tsx
+++ react/admin/Components/GenerateSchema.tsx
@@ -1,7 +1,51 @@
 import React, { FC } from "react";
+import { omsReturnRequest } from '../../common/templates/oms-return-request'
 
 const GenerateSchema: FC<{}> = () => {
   function generateSchema() {
+
+      fetch("/api/template-render/pvt/templates", {
+          method: "POST",
+          body: JSON.stringify({
+              "Name": "oms-return-request",
+              "FriendlyName": "[OMS] Return Request",
+              "Description": null,
+              "IsDefaultTemplate": false,
+              "AccountId": null,
+              "AccountName": null,
+              "ApplicationId": null,
+              "IsPersisted": true,
+              "IsRemoved": false,
+              "Type": "",
+              "Templates": {
+                  "email": {
+                      "To": "{{data.email}}",
+                      "CC": null,
+                      "BCC": "{{#compare data.status \"==\" 'New'}}{{/compare}}",
+                      "Subject": "Formular de returnare {{data.DocumentId}}",
+                      "Message": omsReturnRequest,
+                      "Type": "E",
+                      "ProviderId": "00000000-0000-0000-0000-000000000000",
+                      "ProviderName": null,
+                      "IsActive": true,
+                      "withError": false
+                  },
+                  "sms": {
+                      "Type": "S",
+                      "ProviderId": null,
+                      "ProviderName": null,
+                      "IsActive": false,
+                      "withError": false,
+                      "Parameters": []
+                  }
+              }
+          }),
+          headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json"
+          }
+      });
+
     fetch("/returns/generateSchema/", {
       method: "PUT",
       headers: {
