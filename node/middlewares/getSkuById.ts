c2b10b06bcc4beccfd3846d99ba7ba4f391c0ce0
Cristian Longo <cristian.longo@customsoft.ro>
2021-05-04 09:50:42 +0300

    ref-53568
    New fields on products

---
+++ node/middlewares/getSkuById.ts
@@ -0,0 +1,13 @@
+export async function getSkuById(ctx: Context, next: () => Promise<any>) {
+
+    const {
+        clients: {returnApp: returnAppClient}
+    } = ctx
+    const { id } = ctx.vtex.route.params
+    const response = await returnAppClient.getSkuById(ctx, id)
+
+    ctx.status = 200
+    ctx.body = response
+
+    await next()
+}
