5ccd61efa5af3c60c468900a45046b87cdc4b8de
Asem Qaffaf <asem@qaffaf.com>
2022-02-07 13:13:18 +0200

    fix enableOtherOption name in messages

--- react/admin/ReturnsSettings.tsx
+++ react/admin/ReturnsSettings.tsx
@@ -53,7 +53,7 @@ const messages = defineMessages({
   all: { id: 'returns.all' },
   saveSettings: { id: 'settings.saveSettings' },
   otherOptions: { id: 'settings.otherOptions' },
-  enableOtherOptions: { id: 'settings.otherOptions_enable' },
+  enableOtherOptions: { id: 'settings.enableOtherOption' },
 })
 
 class ReturnsSettings extends Component<any, any> {
