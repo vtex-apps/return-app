cc2ce23d59c28036b3be1e30fbc3cdaa69f394e9
Ezequias Calvo <ezequias.calvo@vtex.com.br>
2022-03-04 08:32:07 +0100

    feat: delete unnecessary try catch

--- node/middlewares/getSkuById.ts
+++ node/middlewares/getSkuById.ts
@@ -6,29 +6,19 @@ export async function getSkuById(ctx: Context, next: () => Promise<any>) {
 
   const { id } = ctx.vtex.route.params
 
-  try {
-    const response = await returnAppClient.getSkuById(ctx, id)
+  const response = await returnAppClient.getSkuById(ctx, id)
 
-    if (!response) {
-      throw new Error(`Error getting SkuById for id: ${id}`)
-    }
-
-    logger.info({
-      message: 'Get SKU by id successfully',
-      data: response,
-    })
-
-    ctx.status = 200
-    ctx.body = response
-  } catch (e) {
-    logger.error({
-      message: `Error getting SKU by id: ${id}`,
-      error: e,
-      data: {
-        ctx,
-      },
-    })
+  if (!response) {
+    throw new Error(`Error getting SkuById for id: ${id}`)
   }
+
+  logger.info({
+    message: 'Get SKU by id successfully',
+    data: response,
+  })
+
+  ctx.status = 200
+  ctx.body = response
 
   await next()
 }
