f27955d252f949aab0ad01d58ffe2df0418451b0
Filadelfo <filadelfo.braz@gmail.com>
2022-03-29 16:32:26 +0200

    Feat: Validate payment types allowed on mutation to app settings

--- node/resolvers/appSettings.ts
+++ node/resolvers/appSettings.ts
@@ -1,9 +1,30 @@
 import type {
   ReturnAppSettings,
   MutationSaveReturnAppSettingsArgs,
+  PaymentOptions,
+  PaymentType,
 } from 'vtex.return-app'
 
 const SETTINGS_PATH = 'app-settings'
+
+const validatePaymentOptions = (paymentOptions: PaymentOptions) => {
+  const { enablePaymentMethodSelection, allowedPaymentTypes } = paymentOptions
+
+  // If the user has not enabled the payment method selection, then the allowed payment types can be all unselected.
+  if (!enablePaymentMethodSelection) return true
+
+  let result = false
+
+  for (const paymentType of Object.keys(allowedPaymentTypes)) {
+    // If we have at least one payment method selected, then the payment options are valid.
+    if (allowedPaymentTypes[paymentType as keyof PaymentType]) {
+      result = true
+      break
+    }
+  }
+
+  return result
+}
 
 const returnAppSettings = (
   _root: unknown,
@@ -25,6 +46,11 @@ const saveReturnAppSettings = async (
   const {
     clients: { appSettings },
   } = ctx
+
+  // validate that there is at least one payment method selected or user has to use the same as in the order
+  if (!validatePaymentOptions(args.settings.paymentOptions)) {
+    throw new Error('At least one payment method must be selected')
+  }
 
   await appSettings.save(SETTINGS_PATH, args.settings)
 
