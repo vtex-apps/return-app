e70604c100f4ab10938b6f92f66ee650558d69c1
Filadelfo <filadelfo.braz@gmail.com>
2022-04-04 12:22:25 +0200

    Feat: Add resolver to get order related to a client, with pagination info

--- node/resolvers/ordersAvailableToReturn.ts
+++ node/resolvers/ordersAvailableToReturn.ts
@@ -1,14 +1,70 @@
+import { ResolverError } from '@vtex/api'
+import type { OrderToReturnList } from 'vtex.return-app'
+
+import { SETTINGS_PATH } from '../utils/constants'
+
+const currentDate = new Date().toISOString()
+
+const substractDays = (days: number) => {
+  const date = new Date()
+
+  date.setDate(date.getDate() - days)
+  date.setUTCHours(0, 0, 0)
+
+  return date.toISOString()
+}
+
+const createParams = ({
+  maxDays,
+  userEmail,
+  page = 1,
+}: {
+  maxDays: number
+  userEmail: string
+  page: number
+}) => ({
+  clientEmail: userEmail,
+  orderBy: 'creationDate,desc' as const,
+  f_status: 'invoiced' as const,
+  f_creationDate: `creationDate:[${substractDays(maxDays)} TO ${currentDate}]`,
+  page,
+  perPage: 15 as const,
+})
+
 export const ordersAvailableToReturn = async (
   _: unknown,
-  _args: unknown,
+  args: { page: number },
   ctx: Context
-) => {
+): Promise<OrderToReturnList | null> => {
   const {
     state: { userProfile },
+    clients: { appSettings, oms },
   } = ctx
 
-  // eslint-disable-next-line no-console
-  console.log({ userProfile })
+  const { page } = args
 
-  return true
+  const settings = await appSettings.get(SETTINGS_PATH, true)
+
+  if (!settings) {
+    throw new ResolverError('Return App settings is not configured')
+  }
+
+  const { maxDays } = settings
+  const { email } = userProfile
+
+  const { list, paging } = await oms.listOrdersWithParams(
+    createParams({ maxDays, userEmail: email, page })
+  )
+
+  // 1. fetch order details to get items and packages
+  // 2. calculate all items already shipped - packages invoice type output
+  // 3. calculate all items already returned - packages invoice type input
+  // 4. Get items committed to be returned (will need to wait until feature the create RMA is done)
+  // 5. calculate all items still available to be returned (2 - (3 + 4))
+
+  if (!list || !paging) {
+    return null
+  }
+
+  return { list, paging }
 }
---
+++ node/utils/constants.ts
@@ -0,0 +1,1 @@
+export const SETTINGS_PATH = 'app-settings'
