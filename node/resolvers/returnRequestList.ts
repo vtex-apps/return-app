7299bf807e1e756ad25b28946883aa306bbe2bbe
Filadelfo <filadelfo.braz@gmail.com>
2022-05-06 14:34:17 +0200

    Feat: Add function to build where clause based on query arguments and user id

--- node/resolvers/returnRequestList.ts
+++ node/resolvers/returnRequestList.ts
@@ -1,4 +1,51 @@
-import type { QueryReturnRequestListArgs } from 'vtex.return-app'
+import type {
+  QueryReturnRequestListArgs,
+  ReturnRequestFilters,
+  Maybe,
+} from 'vtex.return-app'
+
+const filterDate = (date: string): string => {
+  const newDate = new Date(date)
+  const day = newDate.getDate()
+  const month = newDate.getMonth() + 1
+  const year = newDate.getFullYear()
+
+  return `${year}-${month < 10 ? `0${month}` : `${month}`}-${
+    day < 10 ? `0${day}` : `${day}`
+  }`
+}
+
+const buildWhereClause = (
+  filter: Maybe<ReturnRequestFilters> | undefined,
+  userId?: string
+) => {
+  const userFilter = userId && `customerProfileData.userId = "${userId}"`
+
+  if (!filter) return userFilter
+
+  const returnFilters = Object.entries(filter)
+  const whereFilter = returnFilters.reduce((where, [key, value]) => {
+    if (!value) return where
+
+    if (where.length) {
+      where += ` AND `
+    }
+
+    if (key === 'createdIn' && typeof value !== 'string') {
+      where += `${key} between ${filterDate(value.from)} AND ${filterDate(
+        value.to
+      )}`
+
+      return where
+    }
+
+    where += `${key}=${value}`
+
+    return where
+  }, '')
+
+  return userFilter ? `${userFilter} AND ${whereFilter}` : whereFilter
+}
 
 export const returnRequestList = async (
   _: unknown,
@@ -7,11 +54,22 @@ export const returnRequestList = async (
 ) => {
   const {
     clients: { returnRequest: returnRequestClient },
+    request: { header },
+    state: { userProfile },
   } = ctx
+
+  const { page, filter } = args
+  const { userId } = userProfile
+
+  // vtexProduct is undefined when coming from GraphQL IDE
+  const vtexProduct = header['x-vtex-product'] as 'admin' | 'store' | undefined
+
+  // This is useful to apply user filter when an admin is impersonating a store user.
+  const filterUser = vtexProduct === 'store' ? userId : undefined
 
   const rmaSearchResult = await returnRequestClient.searchRaw(
     {
-      page: args.page,
+      page,
       pageSize: 25,
     },
     [
@@ -23,18 +81,19 @@ export const returnRequestList = async (
       'dateSubmitted',
       'userComment',
     ],
-    'createdIn DESC'
+    'createdIn DESC',
+    buildWhereClause(filter, filterUser)
   )
 
   const { data, pagination } = rmaSearchResult
-  const { page, pageSize, total } = pagination
+  const { page: currentPage, pageSize, total } = pagination
 
   return {
     list: data,
     paging: {
       total,
       perPage: pageSize,
-      currentPage: page,
+      currentPage,
       pages: Math.ceil(total / pageSize),
     },
   }
