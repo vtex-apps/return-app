9264a25d3af2cab61f0bfa8677f629b0375c3f36
Filadelfo <filadelfo.braz@gmail.com>
2022-05-09 18:18:04 +0200

    Feat: Render return list on admin side

--- react/admin/AdminReturnList.tsx
+++ react/admin/AdminReturnList.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Link } from 'vtex.render-runtime'
-import { Button } from 'vtex.styleguide'
+import { Link } from 'vtex.styleguide'
 
-import { useReturnRequestList } from '../../hooks/useReturnRequestList'
+import { useReturnRequestList } from '../hooks/useReturnRequestList'
 
-export const ReturnRequestList = () => {
+export const AdminReturnList = () => {
   const { returnRequestData } = useReturnRequestList()
   const { data, loading, error } = returnRequestData
   const { returnRequestList } = data ?? {}
@@ -16,16 +15,12 @@ export const ReturnRequestList = () => {
   return loading ? null : (
     <div>
       <p>Total Return {paging?.total}</p>
-      <div>
-        <Button variation="primary" size="small" href="#/my-returns/add">
-          Add new
-        </Button>
-      </div>
       {list?.map((returnRequest) => (
         <div key={returnRequest.id}>
           <p>Id: {returnRequest.id}</p>
           <p>Status: {returnRequest.status}</p>
-          <p>Sequence number: {returnRequest.sequenceNumber}</p>
+          <p>Sequence Number: {returnRequest.sequenceNumber}</p>
+          <p>Order id: {returnRequest.orderId}</p>
           <p>Created at: {returnRequest.createdIn}</p>
           <Link href={`#/my-returns/details/${returnRequest.id}`}>
             See details
