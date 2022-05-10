343bc20c29f9ce68623582a271007b157e991c39
Ezequias Calvo <ezequias.calvo@vtex.com.br>
2022-05-10 13:11:36 +0200

    feat: add condition and render

--- react/store/createReturnRequest/components/ReturnInformationTable.tsx
+++ react/store/createReturnRequest/components/ReturnInformationTable.tsx
@@ -25,21 +25,45 @@ export const ReturnInformationTable = ({ items, selectedItems }: Props) => {
         </tr>
       </thead>
       <tbody className="v-mid">
-        {selectedItems.map(({ quantity, orderItemIndex }) => {
-          return quantity ? (
-            <tr className="ph5">
-              <td className="w-50 pv5">
-                <div className="flex ml2">
-                  <img src={items[orderItemIndex].imageUrl} alt="Product" />
-                  <p>{items[orderItemIndex].name}</p>
-                </div>
-              </td>
-              <td className="w-50 tc pv5">
-                <p>{quantity}</p>
-              </td>
-            </tr>
-          ) : null
-        })}
+        {selectedItems.map(
+          ({ quantity, orderItemIndex, condition, returnReason }) => {
+            return quantity ? (
+              <tr className="ph5">
+                <td className="w-50 pv5">
+                  <div className="flex items-center ml2">
+                    <div className="mr3">
+                      <img src={items[orderItemIndex].imageUrl} alt="Product" />
+                    </div>
+                    <div>
+                      <p className="b">{items[orderItemIndex].name}</p>
+                      <div className="flex">
+                        <p className="f6 mt0 mr3 gray b">
+                          <FormattedMessage id="store/return-app.return-information-table.table-row.p-condition" />
+                        </p>
+                        <p className="f6 mt0 gray ">{condition}</p>
+                      </div>
+                      <div className="flex">
+                        <p className="f6 mv0 mr3 gray b">
+                          {' '}
+                          <FormattedMessage id="store/return-app.return-information-table.table-row.p-reason" />{' '}
+                        </p>
+                        <p className="f6 mv0 gray ">
+                          {returnReason?.reason}{' '}
+                          {returnReason?.otherReason
+                            ? returnReason?.otherReason
+                            : null}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td className="w-50 tc pv5">
+                  <p>{quantity}</p>
+                </td>
+              </tr>
+            ) : null
+          }
+        )}
       </tbody>
     </table>
   )
