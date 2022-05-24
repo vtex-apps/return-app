b9e63506edf03f2d7a7d582ef89ead7ac3750531
Ezequias Calvo <ezequias.calvo@vtex.com.br>
2022-05-24 10:54:01 +0200

    refactor: reuse payment messages

--- react/store/utils/defaultPaymentMethodsMessages.ts
+++ react/store/utils/defaultPaymentMethodsMessages.ts
@@ -7,6 +7,9 @@ export const defaultPaymentMethodsMessages = defineMessages({
   giftCard: {
     id: 'store/return-app.return-order-details.payment-options.gift-card',
   },
+  bank: {
+    id: 'store/return-app.return-order-details.payment-options.bank',
+  },
   sameAsPurchase: {
     id: 'store/return-app.confirm-payment-methods.refund-method.p-same-as-purchase',
   },
