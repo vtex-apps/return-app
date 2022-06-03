5b5e4dabcc2e1393d03b70d9c1ddb07a23b1bc3a
Filadelfo <filadelfo.braz@gmail.com>
2022-06-03 14:24:05 +0200

    Refactor: Move mutation to provider to make easy to update cache after calling it

--- react/admin/provider/ReturnDetailsProvider.tsx
+++ react/admin/provider/ReturnDetailsProvider.tsx
@@ -1,20 +1,35 @@
 import type { FC } from 'react'
 import React, { createContext } from 'react'
 import type { ApolloError } from 'apollo-client'
-import { useQuery } from 'react-apollo'
+import { useQuery, useMutation } from 'react-apollo'
 import type {
   ReturnRequestResponse,
   QueryReturnRequestArgs,
+  RefundStatusData,
+  MutationUpdateReturnRequestStatusArgs,
+  ReturnRequestCommentInput,
+  Status,
 } from 'vtex.return-app'
+import { FormattedMessage } from 'react-intl'
 
 import GET_REQUEST_DETAILS_ADMIN from '../graphql/getRequestDetailsAdmin.gql'
+import UPDATE_RETURN_STATUS from '../graphql/updateReturnRequestStatus.gql'
+import { useAlert } from '../hooks/userAlert'
 
 interface ReturnDetailsSetupInterface {
   data?: { returnRequestDetails: ReturnRequestResponse }
   loading: boolean
   error?: ApolloError
+  submitting: boolean
+  handleStatusUpdate: (args: HandleStatusUpdateArgs) => Promise<void>
 }
 
+interface HandleStatusUpdateArgs {
+  status: Status
+  id: string
+  comment?: ReturnRequestCommentInput
+  cleanUp?: () => void
+}
 export interface CustomRouteProps {
   requestId: string
 }
@@ -27,6 +42,7 @@ export const ReturnDetailsProvider: FC<CustomRouteProps> = ({
   requestId,
   children,
 }) => {
+  const { openAlert } = useAlert()
   const { data, loading, error } = useQuery<
     { returnRequestDetails: ReturnRequestResponse },
     QueryReturnRequestArgs
@@ -37,8 +53,49 @@ export const ReturnDetailsProvider: FC<CustomRouteProps> = ({
     skip: !requestId,
   })
 
+  const [updateReturnStatus, { loading: submitting }] = useMutation<
+    {
+      updateReturnRequestStatus: RefundStatusData
+    },
+    MutationUpdateReturnRequestStatusArgs
+  >(UPDATE_RETURN_STATUS)
+
+  const handleStatusUpdate = async (args: HandleStatusUpdateArgs) => {
+    const { id, status, comment, cleanUp } = args
+
+    try {
+      const { errors, data: mutationData } = await updateReturnStatus({
+        variables: {
+          requestId: id,
+          status,
+          ...(comment ? { comment } : {}),
+        },
+      })
+
+      if (errors) {
+        throw new Error('Error updating return request status')
+      }
+
+      openAlert(
+        'success',
+        <FormattedMessage id="admin/return-app.return-request-details.update-status.alert.success" />
+      )
+
+      cleanUp?.()
+      // eslint-disable-next-line no-console
+      console.log({ mutationData })
+    } catch {
+      openAlert(
+        'error',
+        <FormattedMessage id="admin/return-app.return-request-details.update-status.alert.error" />
+      )
+    }
+  }
+
   return (
-    <ReturnDetailsContext.Provider value={{ data, loading, error }}>
+    <ReturnDetailsContext.Provider
+      value={{ data, loading, error, submitting, handleStatusUpdate }}
+    >
       {children}
     </ReturnDetailsContext.Provider>
   )
