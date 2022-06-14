715d85f7109e30990f46636171ff44112c76d0c4
Filadelfo <filadelfo.braz@gmail.com>
2022-06-14 17:10:28 +0200

    Feat: Add Loader component for admin components

--- react/admin/ReturnDetails/ReturnDetailsContainer.tsx
+++ react/admin/ReturnDetails/ReturnDetailsContainer.tsx
@@ -1,23 +1,18 @@
 import React, { useState } from 'react'
-import {
-  Layout,
-  PageHeader,
-  PageBlock,
-  Spinner,
-  EmptyState,
-} from 'vtex.styleguide'
+import { Layout, PageHeader, PageBlock } from 'vtex.styleguide'
 import { FormattedMessage } from 'react-intl'
 import { useRuntime } from 'vtex.render-runtime'
 
 import { UpdateRequestStatus } from './components/UpdateRequestStatus'
 import { useReturnDetails } from '../hooks/useReturnDetails'
 import { VerifyItemsPage } from './components/VerifyItems/VerifyItemsPage'
+import { AdminLoader } from '../AdminLoader'
 
 type Pages = 'return-details' | 'verify-items'
 
 export const ReturnDetailsContainer = () => {
   const [detailsPage, setDetailsPage] = useState<Pages>('return-details')
-  const { data, error, loading } = useReturnDetails()
+  const returnDetails = useReturnDetails()
 
   const { navigate } = useRuntime()
 
@@ -45,21 +40,20 @@ export const ReturnDetailsContainer = () => {
       }
     >
       <PageBlock variation="full" fit="fill">
-        {error ? (
-          <EmptyState
-            title={
+        <AdminLoader
+          {...returnDetails}
+          errorMessages={{
+            errorTitle: (
               <FormattedMessage id="admin/return-app.return-request-details.error.title" />
-            }
-          >
-            <FormattedMessage id="admin/return-app.return-request-details.error.description" />
-          </EmptyState>
-        ) : loading ? (
-          <Spinner />
-        ) : !data?.returnRequestDetails ? null : (
+            ),
+            errorDescription: (
+              <FormattedMessage id="admin/return-app.return-request-details.error.description" />
+            ),
+          }}
+        >
           <>
             {detailsPage !== 'return-details' ? null : (
               <>
-                <div>Status {data.returnRequestDetails.status}</div>
                 <UpdateRequestStatus
                   onViewVerifyItems={() =>
                     handleViewVerifyItems('verify-items')
@@ -75,7 +69,7 @@ export const ReturnDetailsContainer = () => {
               />
             )}
           </>
-        )}
+        </AdminLoader>
       </PageBlock>
     </Layout>
   )
