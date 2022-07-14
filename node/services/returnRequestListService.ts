30a3f1fe27eeedc71b084252e4c4e7e7bf1d8b0d
Filadelfo <filadelfo.braz@gmail.com>
2022-07-14 17:56:30 -0300

    Refactor: Ensure a non admin user cannot use other user info to get their requests

--- node/services/returnRequestListService.ts
+++ node/services/returnRequestListService.ts
@@ -75,22 +75,27 @@ export const returnRequestListService = async (
 
   const { userId: userIdArg, userEmail: userEmailArg } = filter ?? {}
 
-  const userId = userIdArg ?? userIdProfile
-  const userEmail = userEmailArg ?? userEmailProfile
+  const userIsAdmin = Boolean(appkey) || role === 'admin'
 
-  // vtexProduct is undefined when coming from GraphQL IDE
+  // only admin users can pass userId or userEmail in the request.
+  // For non admin users, the userId or userEmail must be gotten from cookie session.
+  // Otherwise, a non admin user could search for another user's return requests
+  const userId = userIsAdmin ? userIdArg || userIdProfile : userIdProfile
+  const userEmail = userIsAdmin
+    ? userEmailArg || userEmailProfile
+    : userEmailProfile
+
+  // vtexProduct is undefined when coming from GraphQL IDE or from a external request
   const vtexProduct = header['x-vtex-product'] as 'admin' | 'store' | undefined
 
-  // This is useful to apply user filter when an admin is impersonating a store user.
-  const requireFilterByUser = vtexProduct === 'store' || role === 'store-user'
-
-  const userIsAdmin = Boolean(appkey) || role === 'admin'
+  // When the user is not admin or the request is coming from the store, we need to apply the user filter to get the right requests
+  const requireFilterByUser =
+    !userIsAdmin || vtexProduct === 'store' || role === 'store-user'
 
   const hasUserIdOrEmail = Boolean(userId || userEmail)
 
-  // If user is not admin it's necessary to filter the query by userId or userEmail
-  if (!userIsAdmin && requireFilterByUser && !hasUserIdOrEmail) {
-    throw new ForbiddenError('User cannot access this request')
+  if (requireFilterByUser && !hasUserIdOrEmail) {
+    throw new ForbiddenError('Missing params to filter by store user')
   }
 
   const adjustedFilter = requireFilterByUser
