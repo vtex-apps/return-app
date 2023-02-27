acf9062570de2fb7ccba13f69c751e4adee3d04c
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-02-27 13:03:07 -0400

    refactor: change table in mobile to card in DetailsSolicitation

--- react/common/components/ReturnDetails/ItemDetails/ItemDetailsList.tsx
+++ react/common/components/ReturnDetails/ItemDetails/ItemDetailsList.tsx
@@ -12,10 +12,12 @@ import { useIntl } from 'react-intl'
 
 import { useReturnDetails } from '../../../hooks/useReturnDetails'
 import { itemDetailsSchema } from './itemDetailsSchema'
+import { ItemDetailsCard } from './ItemDetailsCard'
 
 type ItemStatus = 'new' | 'denied' | 'approved' | 'partiallyApproved'
 
 const CSS_HANDLES = ['itemDetailsListContainer'] as const
+const CSS_HANDLES_PHONE = ['itemDetailsListWrapper'] as const
 
 export interface ItemStatusInterface {
   status: ItemStatus
@@ -84,6 +86,7 @@ const getItemVerificationStatus = (
 
 export const ItemDetailsList = () => {
   const handles = useCssHandles(CSS_HANDLES)
+  const handlesPhone = useCssHandles(CSS_HANDLES_PHONE)
   const { formatMessage } = useIntl()
   const { data } = useReturnDetails()
   const {
@@ -102,6 +105,25 @@ export const ItemDetailsList = () => {
   )
 
   const { currencyCode } = cultureInfoData
+
+  if (phone) {
+    return (
+      <section
+        className={`${handlesPhone.itemDetailsListWrapper} mv4 flex flex-column`}
+      >
+        {items?.map((item) => {
+          return (
+            <ItemDetailsCard
+              currencyCode={currencyCode}
+              key={item.orderItemIndex}
+              item={item}
+              itemsVerificationStatus={itemsVerificationStatus}
+            />
+          )
+        })}
+      </section>
+    )
+  }
 
   return (
     <section className={handles.itemDetailsListContainer}>
