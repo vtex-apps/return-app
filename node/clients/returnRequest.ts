028188f3babde9827a54c55fd1fecf6741267c13
miguel-carrera <miguel.carrera@vtex.com.br>
2025-06-05 12:05:23 +0200

    Reimplement the returnRequest client to fix the schema

--- node/clients/returnRequest.ts
+++ node/clients/returnRequest.ts
@@ -1,17 +1,26 @@
-import type { InstanceOptions, IOContext, IOResponse } from '@vtex/api'
-import { JanusClient } from '@vtex/api'
-import { masterDataFor } from '@vtex/clients'
+import type { InstanceOptions, IOContext } from '@vtex/api'
+import { JanusClient, MasterData } from '@vtex/api'
 import type {
   DocumentResponse,
-  MasterDataEntity,
   ScrollInput,
   WithMetadata,
 } from '@vtex/clients/build/clients/masterData/MasterDataEntity'
 import type { ReturnRequest } from 'odp.return-app'
 
-export const DATA_ENTITY_NAME = 'odp_return_app_returnRequest'
+const DATA_ENTITY_NAME = 'odp_return_app_returnRequest'
+const SCHEMA_NAME = 'odp.returns'
+
+interface PaginationArgs {
+  page: number
+  pageSize: number
+}
 
 export default class ReturnRequestClient extends JanusClient {
+  public dataEntity: string
+  public schema: string
+  private inner: MasterData
+  // private masterDataEntity: MasterDataEntity<ReturnRequest>
+
   constructor(context: IOContext, options?: InstanceOptions) {
     super(context, {
       ...options,
@@ -23,111 +32,176 @@ export default class ReturnRequestClient extends JanusClient {
         'Content-Type': 'application/json',
       },
     })
-  }
 
-  private getMasterDataClient = (): MasterDataEntity<ReturnRequest> => {
-    const MasterDataBuilder = masterDataFor<ReturnRequest>(
+    this.schema = SCHEMA_NAME
+    this.dataEntity = DATA_ENTITY_NAME
+
+    this.inner = new MasterData(context, options)
+
+    /* 
+    const MasterDataEntityBuilder = masterDataFor<ReturnRequest>(
       'returnRequest',
       undefined,
       1
     )
 
-    const masterDataClient = new MasterDataBuilder(this.context)
-
-    masterDataClient.schema = '3.10.0'
-
-    return masterDataClient
+    this.masterDataEntity = new MasterDataEntityBuilder(context)
+    this.masterDataEntity.schema = SCHEMA_NAME 
+    */
   }
 
-  private paginationArgsToHeaders({ page, pageSize }: any) {
-    if (page < 1) {
-      page = 1
-    }
-
-    const startIndex = (page - 1) * pageSize
-    const endIndex = Number(startIndex) + Number(pageSize)
-
-    return {
-      'REST-Range': `resources=${startIndex}-${endIndex}`,
-    }
+  /*
+  public async _get(id: string, fields: string[]): Promise<any> {
+    return this.masterDataEntity.get(id, fields)
   }
 
-  public schema = ''
-  public dataEntity = DATA_ENTITY_NAME
-
-  public async get(id: string, fields: string[]): Promise<any> {
-    const fieldsStr = fields?.join(',') ?? '_all'
-
-    return this.http.get(
-      `/api/dataentities/${DATA_ENTITY_NAME}/documents/${id}?_fields=${fieldsStr}`
-    )
-  }
-
-  public async search(
+  public async _search(
     pagination: { page: number; pageSize: number },
     fields: string[],
     sort?: string,
     where?: string
   ): Promise<any> {
-    const { page, pageSize } = pagination ?? {}
-    const fieldsStr = fields?.join(',') ?? '_all'
-    const sortStr = sort ?? ''
-    const whereStr = where ?? ''
-
-    return this.http.get(
-      `/api/dataentities/${DATA_ENTITY_NAME}/search?_where=${whereStr}&_fields=${fieldsStr}&_sort=${sortStr}`,
-      {
-        headers: this.paginationArgsToHeaders({ page, pageSize }),
-      }
-    )
+    return this.masterDataEntity.search(pagination, fields, sort, where)
   }
 
-  public async searchRaw(
+  public async _searchRaw(
     pagination: { page: number; pageSize: number },
     fields: string[],
     sort?: string,
     where?: string
   ): Promise<any> {
-    const { page, pageSize } = pagination ?? {}
-    const fieldsStr = fields.join(',')
-    const sortStr = sort ?? ''
-    const whereStr = where ?? ''
-
-    const result = await this.http.getRaw(
-      `/api/dataentities/${DATA_ENTITY_NAME}/search?_where=${whereStr}&_fields=${fieldsStr}&_sort=${sortStr}`,
-      {
-        headers: this.paginationArgsToHeaders({ page, pageSize }),
-      }
-    )
-
-    const { headers } = result
-    const restContentRange = headers['rest-content-range']
-    const total = Number(restContentRange.split('/')[1])
-    const paginationInfo = { ...pagination, total }
-
-    return { data: result.data, pagination: paginationInfo }
+    return this.masterDataEntity.searchRaw(pagination, fields, sort, where)
   }
 
-  public async save(entity: ReturnRequest): Promise<DocumentResponse> {
-    return this.getMasterDataClient().save(entity)
+  public async _save(entity: ReturnRequest): Promise<DocumentResponse> {
+    return this.masterDataEntity.save(entity)
   }
 
-  public async update(id: string, entity: ReturnRequest): Promise<void> {
-    return this.getMasterDataClient().update(id, entity)
+  public async _update(id: string, entity: ReturnRequest): Promise<void> {
+    return this.masterDataEntity.update(id, entity)
   }
 
-  public async delete(id: string): Promise<IOResponse<void>> {
-    return this.getMasterDataClient().delete(id)
+  public async _delete(id: string): Promise<IOResponse<void>> {
+    return this.masterDataEntity.delete(id)
   }
 
-  public async saveOrUpdate(entity: any): Promise<DocumentResponse> {
-    return this.getMasterDataClient().saveOrUpdate(entity)
+  public async _saveOrUpdate(entity: any): Promise<DocumentResponse> {
+    return this.masterDataEntity.saveOrUpdate(entity)
   }
 
-  public async scroll(input: ScrollInput<ReturnRequest>): Promise<{
+  public async _scroll(input: ScrollInput<ReturnRequest>): Promise<{
     data: Array<Pick<WithMetadata<ReturnRequest>, string | number>>
     mdToken: string
   }> {
-    return this.getMasterDataClient().scroll(input)
+    return this.masterDataEntity.scroll(input)
+  }
+  */
+
+  public async save(entity: ReturnRequest): Promise<DocumentResponse> {
+    return this.inner.createDocument({
+      dataEntity: this.dataEntity,
+      fields: entity,
+      schema: this.schema,
+    })
+  }
+
+  public async update(id: string, fields: Partial<ReturnRequest>) {
+    return this.inner.updatePartialDocument({
+      dataEntity: this.dataEntity,
+      id,
+      fields,
+      schema: this.schema,
+    })
+  }
+
+  public async saveOrUpdate(
+    fields: ReturnRequest & { id: string }
+  ): Promise<DocumentResponse> {
+    return this.inner.createOrUpdateEntireDocument({
+      dataEntity: this.dataEntity,
+      fields,
+      schema: this.schema,
+    })
+  }
+
+  public async saveOrUpdatePartial(
+    fields: ReturnRequest & { id: string }
+  ): Promise<DocumentResponse> {
+    return this.inner.createOrUpdatePartialDocument({
+      dataEntity: this.dataEntity,
+      fields,
+      schema: this.schema,
+    })
+  }
+
+  public async delete(id: string) {
+    return this.inner.deleteDocument({ dataEntity: this.dataEntity, id })
+  }
+
+  // eslint-disable-next-line max-params
+  public async search<K extends keyof WithMetadata<ReturnRequest>>(
+    pagination: PaginationArgs,
+    fields: Array<ThisType<K> | '_all'>,
+    sort?: string,
+    where?: string
+  ): Promise<Array<Pick<WithMetadata<ReturnRequest>, K>>> {
+    return this.inner.searchDocuments<Pick<WithMetadata<ReturnRequest>, K>>({
+      dataEntity: this.dataEntity,
+      pagination,
+      fields: fields.map((field) => field.toString()),
+      sort,
+      where,
+      schema: this.schema,
+    })
+  }
+
+  // eslint-disable-next-line max-params
+  public async searchRaw<K extends keyof WithMetadata<ReturnRequest>>(
+    pagination: PaginationArgs,
+    fields: Array<ThisType<K> | '_all'>,
+    sort?: string,
+    where?: string
+  ): Promise<{
+    data: Array<Pick<WithMetadata<ReturnRequest>, K>>
+    pagination: { total: number; page: number; pageSize: number }
+  }> {
+    return this.inner.searchDocumentsWithPaginationInfo({
+      dataEntity: this.dataEntity,
+      pagination,
+      fields: fields.map((field) => field.toString()),
+      sort,
+      where,
+      schema: this.schema,
+    })
+  }
+
+  public async get<K extends keyof WithMetadata<ReturnRequest>>(
+    id: string,
+    fields: Array<ThisType<K> | '_all'>
+  ) {
+    return this.inner.getDocument<Pick<WithMetadata<ReturnRequest>, K>>({
+      dataEntity: this.dataEntity,
+      id,
+      fields: fields.map((field) => field.toString()),
+    })
+  }
+
+  public async scroll<K extends keyof WithMetadata<ReturnRequest>>(
+    input: ScrollInput<K>
+  ) {
+    const { mdToken, data } = await this.inner.scrollDocuments({
+      ...input,
+      dataEntity: this.dataEntity,
+      fields: input.fields.map((field) => field.toString()),
+      schema: this.schema,
+    })
+
+    /**
+     * The scroll method on Master Data's client is mistyped (duplicating the object)
+     */
+    return {
+      mdToken,
+      data: data as unknown as Array<Pick<WithMetadata<ReturnRequest>, K>>,
+    }
   }
 }
